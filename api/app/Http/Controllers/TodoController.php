e7b3dc7bbd78eb335cbca4a577fe01b41150e05a
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-19 22:27:41 -0300

    concluindo crud

--- api/app/Http/Controllers/TodoController.php
+++ api/app/Http/Controllers/TodoController.php
@@ -32,24 +32,46 @@ class TodoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Todo $todo)
+    public function show($id)
     {
-        //
+        $todo = Todo::findOrFail($id); //Todo::find($id);
+        return response($todo, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Todo $todo)
+    public function update(Request $request, $id)
     {
-        //
+        $validation = $request->validate([
+            'name' => 'required',
+            'responsible' => 'required'
+        ]);
+        $todo = Todo::findOrFail($id);
+        $todo->update($validation);
+        return response($todo, 200);
     }
-
+    public function updateStatus($id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update([
+           'status' => $todo['status'] === 0 ? 1 : 0
+        ]);
+        return response($todo,200);
+    }
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Todo $todo)
+    public function destroy(Request $request)
     {
-        //
+        try {
+            $validation = $request->validate([
+                'ids' => ['required','array']
+            ]);
+            Todo::destroy($validation['ids']);
+            return response(['success' => true],200);
+        } catch (\Error $e) {
+            return response($e, 200);
+        }
     }
 }
