fb5f046938ef05d114b7c6a55561e88ae238f085
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-20 21:36:21 -0300

    paginação

--- api/app/Http/Controllers/TodoController.php
+++ api/app/Http/Controllers/TodoController.php
@@ -12,7 +12,7 @@ class TodoController extends Controller
      */
     public function index()
     {
-        $todos = Todo::all();
+        $todos = Todo::paginate(5);
         return response($todos, 200);
     }
 
